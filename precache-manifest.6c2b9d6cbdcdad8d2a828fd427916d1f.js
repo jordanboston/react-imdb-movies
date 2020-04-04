429b24d200a76d80a7ac007bc3521a05cfd0e7cb
Jordan Boston <jordandboston@gmail.com>
2020-04-04 12:14:08 -0600

    Updates

---
+++ precache-manifest.6c2b9d6cbdcdad8d2a828fd427916d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516d2ec418358957a314803280028382",
+    "url": "/react-imdb-movies/index.html"
+  },
+  {
+    "revision": "c37ac62cf491df40d4a4",
+    "url": "/react-imdb-movies/static/css/main.22e82b57.chunk.css"
+  },
+  {
+    "revision": "a13c058266dab3e4aa07",
+    "url": "/react-imdb-movies/static/js/2.2e4ee336.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-imdb-movies/static/js/2.2e4ee336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37ac62cf491df40d4a4",
+    "url": "/react-imdb-movies/static/js/main.e9267533.chunk.js"
+  },
+  {
+    "revision": "eb52336c71b42ce21eef",
+    "url": "/react-imdb-movies/static/js/runtime-main.7c469ba1.js"
+  }
+]);
